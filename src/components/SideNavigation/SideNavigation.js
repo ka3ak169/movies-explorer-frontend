6fb8d388d006d50270146795fe34395324637683
Aleksey Kazakov <ka3akne@mail.ru>
2023-08-21 04:54:58 +0400

    исправить иконку

--- src/components/SideNavigation/SideNavigation.js
+++ src/components/SideNavigation/SideNavigation.js
@@ -3,20 +3,51 @@ import { Link } from "react-router-dom";
 
 function SideNavigation({ isMenuOpen, setMenuOpen }) {
   const closeSideNavigation = () => {
-    setMenuOpen(false)
-  }
+    setMenuOpen(false);
+  };
 
   return (
     <>
       {isMenuOpen && <div className="side-navigation__overlay" />}
-      <div className={`side-navigation ${isMenuOpen ? '' : 'side-navigation_disabled'}`}>
-        <div className="side-navigation__close-btn" onClick={closeSideNavigation}></div>
+      <div
+        className={`side-navigation ${
+          isMenuOpen ? "" : "side-navigation_disabled"
+        }`}
+      >
+        <div
+          className="side-navigation__close-btn"
+          onClick={closeSideNavigation}
+        ></div>
         <nav className="side-navigation__link-container">
-          <Link className="side-navigation__link" to={'/'} onClick={closeSideNavigation}>Главная</Link>
-          <Link className="side-navigation__link" to={'/movies'} onClick={closeSideNavigation}>Фильмы</Link>
-          <Link className="side-navigation__link" to={'/saved-movies'} onClick={closeSideNavigation}>Сохранённые фильмы</Link>
+          <Link
+            className="side-navigation__link"
+            to={"/"}
+            onClick={closeSideNavigation}
+          >
+            Главная
+          </Link>
+          <Link
+            className="side-navigation__link"
+            to={"/movies"}
+            onClick={closeSideNavigation}
+          >
+            Фильмы
+          </Link>
+          <Link
+            className="side-navigation__link"
+            to={"/saved-movies"}
+            onClick={closeSideNavigation}
+          >
+            Сохранённые фильмы
+          </Link>
         </nav>
-        <Link className={`navigation__account-container ${isMenuOpen ? 'navigation__account-container_enabled' : ''}`} to={"/profile"}>
+        <Link
+          className={`navigation__account-container ${
+            isMenuOpen ? "navigation__account-container_enabled" : ""
+          }`}
+          to={"/profile"}
+          onClick={closeSideNavigation}
+        >
           <p className="navigation__link">Аккаунт</p>
           <div className="navigation__logo"></div>
         </Link>
