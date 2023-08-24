2d2e193d20bb7e438a5c0ef6aeb7b7caf671bbe0
Aleksey Kazakov <ka3akne@mail.ru>
2023-08-24 20:17:22 +0400

    перереключить на сервер

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,9 @@
-export const devUrl = 'http://localhost:3000';
-// export const prodUrl = 'https://api.diploma-folio.nomoredomains.xyz';
+// export const devUrl = 'http://localhost:3000';
+export const prodUrl = 'https://api.diploma-folio.nomoredomains.xyz';
 
 export const movieApiUrl = 'https://api.nomoreparties.co/beatfilm-movies';
 
 export const movieApiPart = 'https://api.nomoreparties.co';
 
-export default devUrl;
-// export default prodUrl;
+// export default devUrl;
+export default prodUrl;
